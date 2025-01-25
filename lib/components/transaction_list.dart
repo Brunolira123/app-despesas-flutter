0762511a2b9f471ca1bfcc61bceb1ed303354ee9
Bruno <brunolira1@icloud.com>
2025-01-25 13:12:26 -0300

    Implemented scroolable

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,67 +10,64 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                const SizedBox(height: 20),
-                Text(
-                  'Nenhuma Transação Cadastrada!',
-                  style: Theme.of(context).textTheme.titleLarge,
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              const SizedBox(height: 20),
+              Text(
+                'Nenhuma Transação Cadastrada!',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return Card(
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
                 ),
-              ],
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (ctx, index) {
-                final tr = transactions[index];
-                return Card(
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.purple,
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6),
-                          child: FittedBox(
-                            child: Text(
-                              'R\$${tr.amount.toStringAsFixed(2)}',
-                              style: const TextStyle(
-                                color: Colors.white,
-                              ),
+                child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.purple,
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                            'R\$${tr.amount.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                              color: Colors.white,
                             ),
                           ),
                         ),
                       ),
-                      title: Text(
-                        tr.title,
-                        selectionColor: Colors.purple,
-                      ),
-                      subtitle: Text(
-                        DateFormat('d MMM y').format(tr.date),
-                      ),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        color: Colors.red,
-                        onPressed: () => onRemove(tr.id),
-                      )),
-                );
-              },
-            ),
-    );
+                    ),
+                    title: Text(
+                      tr.title,
+                      selectionColor: Colors.purple,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d MMM y').format(tr.date),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      color: Colors.red,
+                      onPressed: () => onRemove(tr.id),
+                    )),
+              );
+            },
+          );
   }
 }
